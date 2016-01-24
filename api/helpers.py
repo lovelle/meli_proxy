b4590f2ec3217930130301d0e1f7ff50ea941746
Ezequiel Lovelle <ezequiellovelle@gmail.com>
2016-01-24 19:30:50 -0300

    unnecessary code

--- api/helpers.py
+++ api/helpers.py
@@ -14,49 +14,8 @@ from werkzeug import import_string, cached_property
 
 
 class JSONEncoder(BaseJSONEncoder):
-    """Custom :class:`JSONEncoder` which respects objects that include the
-    :class:`JsonSerializer` mixin.
-    """
     def default(self, obj):
-        #if isinstance(obj, JsonSerializer):
-        #    return obj.to_json()
         return super(JSONEncoder, self).default(obj)
-
-"""
-class JsonSerializer(object):
-    A mixin that can be used to mark a SQLAlchemy model class which
-    implements a :func:`to_json` method. The :func:`to_json` method is used
-    in conjuction with the custom :class:`JSONEncoder` class. By default this
-    mixin will assume all properties of the SQLAlchemy model are to be visible
-    in the JSON output. Extend this class to customize which properties are
-    public, hidden or modified before being being passed to the JSON serializer.
-
-
-    __json_public__ = None
-    __json_hidden__ = None
-    __json_modifiers__ = None
-
-    def get_field_names(self):
-        for p in self.__mapper__.iterate_properties:
-            yield p.key
-
-    def to_json(self):
-        field_names = self.get_field_names()
-
-        public = self.__json_public__ or field_names
-        hidden = self.__json_hidden__ or []
-        modifiers = self.__json_modifiers__ or dict()
-
-        rv = dict()
-        for key in public:
-            rv[key] = getattr(self, key)
-        for key, modifier in modifiers.items():
-            value = getattr(self, key)
-            rv[key] = modifier(value, self)
-        for key in hidden:
-            rv.pop(key, None)
-        return rv
-"""
 
 
 class LazyView(object):
@@ -64,7 +23,6 @@ class LazyView(object):
     with a little touch by myself"""
 
     def __init__(self, import_name):
-        print import_name
         self.__module__, self.__name__ = import_name.rsplit('.', 1)
         self.import_name = import_name
 
@@ -85,7 +43,7 @@ class HandleRequest(object):
 
     def set_headers(self, response):
         response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['X-Server'] = 'MeliProxy'
+        response.headers['X-Server'] = app.config.get("SERVER")
         response.headers['X-Response-Time'] = '%.3f' % self.end_timer
         return response
 
